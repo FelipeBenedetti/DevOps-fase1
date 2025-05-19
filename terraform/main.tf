7387e385ae610915fbe4ff1cd64422ed965427d0
Felipe Benedetti <felipe-benedetti@hotmail.com>
2025-05-19 20:20:24 -0300

    add git ignore and fix main

--- terraform/main.tf
+++ terraform/main.tf
@@ -252,36 +252,3 @@ resource "aws_cloudwatch_log_group" "main" {
     Name = "${var.project_name}-log-group"
   }
 }
-
-# Outputs
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  value = aws_subnet.public[*].id
-}
-
-output "private_subnet_ids" {
-  value = aws_subnet.private[*].id
-}
-
-output "ecs_cluster_name" {
-  value = aws_ecs_cluster.main.name
-}
-
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.main.id
-}
-
-output "artifacts_bucket_name" {
-  value = aws_s3_bucket.artifacts.bucket
-}
-
-output "ecs_security_group_id" {
-  value = aws_security_group.ecs.id
-}
-
-output "rds_security_group_id" {
-  value = aws_security_group.rds.id
-}
